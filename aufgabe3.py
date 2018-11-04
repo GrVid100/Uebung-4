60c0a6cfe005e5466540b7ae5df968b59da7a53e
Gregor <Gregor.Vidovic@uni-duesseldorf.de>
2018-11-04 19:40:37 +0100

    Changes of plots

--- aufgabe3.py
+++ aufgabe3.py
@@ -2,14 +2,15 @@ import numpy as np
 import pandas as pd
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 training_set = pd.read_csv("TrainingSet1D.csv", names=['x_vals', 'y_vals'])
 test_set = pd.read_csv("TestSet1D.csv", names=['x_vals', 'y_vals'])
 
-default_degree = 17
-default_noise = 0.015
+default_degree = 18
+default_noise = 0.001
 
+
+# Aufgabenteil a)
 def build_matrix(dataset, degree=default_degree):
     matrix_size = (degree + 1) * dataset.size
     X = np.ones(matrix_size)
@@ -19,6 +20,7 @@ def build_matrix(dataset, degree=default_degree):
     return X
 
 
+# Aufgabenteil b)
 def calculate_MLE_coefficients(degree=default_degree):
     X = build_matrix(training_set['x_vals'], degree)
     X_transposed = X.T
@@ -29,6 +31,18 @@ def calculate_MLE_coefficients(degree=default_degree):
     return coefficients
 
 
+# Aufgabenteil c)
+def calculate_error(dataset, coefficients, degree=default_degree):
+    X = build_matrix(dataset['x_vals'], degree)
+    Xw = np.matmul(X, coefficients)
+    Xw_minus_y = Xw - dataset['y_vals']
+    Xw_minus_y_transposed = Xw_minus_y.T
+    multiplied_matrix = np.matmul(Xw_minus_y_transposed, Xw_minus_y)
+    MSE = multiplied_matrix / dataset['x_vals'].size
+    return MSE
+
+
+# Aufgabenteil f)
 def calculate_RIDGE_coefficients(degree=default_degree, noise=default_noise):
     X = build_matrix(training_set['x_vals'], degree)
     X_transposed = X.T
@@ -40,16 +54,7 @@ def calculate_RIDGE_coefficients(degree=default_degree, noise=default_noise):
     return coefficients
 
 
-def calculate_error(dataset, coefficients, degree=default_degree):
-    X = build_matrix(dataset['x_vals'], degree)
-    Xw = np.matmul(X, coefficients)
-    Xw_minus_y = Xw - dataset['y_vals']
-    Xw_minus_y_transposed = Xw_minus_y.T
-    multiplied_matrix = np.matmul(Xw_minus_y_transposed, Xw_minus_y)
-    MSE = multiplied_matrix/dataset['x_vals'].size
-    return MSE
-
-
+# Make plots
 def determine_y_value(x, coefficients):
     y = 0
     power = 0
@@ -59,89 +64,80 @@ def determine_y_value(x, coefficients):
     return y
 
 
-fig, ax = plt.subplots(3, 2)
+current_error_MLE_training = calculate_error(training_set, calculate_MLE_coefficients())
+current_error_MLE_test = calculate_error(test_set, calculate_MLE_coefficients())
+current_error_RIDGE_training = calculate_error(training_set, calculate_RIDGE_coefficients())
+current_error_RIDGE_test = calculate_error(test_set, calculate_RIDGE_coefficients())
 
-# Make plots
+
+fig, ax = plt.subplots(2, 2)
+fig.tight_layout()
 x_values_for_plots = np.linspace(-5, 5, 100)
-
 y_values_for_plot_training_MLE = [determine_y_value(x, calculate_MLE_coefficients()) for x in
                                   x_values_for_plots]
 ax[0, 0].scatter(training_set['x_vals'], training_set['y_vals'], color='orange')
 ax[0, 0].plot(x_values_for_plots, y_values_for_plot_training_MLE)
 ax[0, 0].set_title('Training Set with MLE Regression')
+ax[0, 0].text(0, 10, 'MSE: ' + str('%.4f' % current_error_MLE_training), style='italic',
+              bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 3})
 
 y_values_for_plot_test_MLE = [determine_y_value(x, calculate_MLE_coefficients()) for x in x_values_for_plots]
 ax[0, 1].scatter(test_set['x_vals'], test_set['y_vals'], color='brown')
 ax[0, 1].plot(x_values_for_plots, y_values_for_plot_test_MLE)
 ax[0, 1].set_title('Test Set with MLE Regression')
+ax[0, 1].text(0, 10, 'MSE: ' + str('%.4f' % current_error_MLE_test), style='italic',
+              bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 3})
 
 y_values_for_plot_training_RIDGE = [determine_y_value(x, calculate_RIDGE_coefficients()) for x in
                                     x_values_for_plots]
 ax[1, 0].scatter(training_set['x_vals'], training_set['y_vals'], color='orange')
 ax[1, 0].plot(x_values_for_plots, y_values_for_plot_training_RIDGE)
 ax[1, 0].set_title('Training Set with RIDGE Regression')
+ax[1, 0].text(0, 10, 'MSE: ' + str('%.4f' % current_error_RIDGE_training), style='italic',
+              bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 3})
 
 y_values_for_plot_test_RIDGE = [determine_y_value(x, calculate_RIDGE_coefficients()) for x in
                                 x_values_for_plots]
 ax[1, 1].scatter(test_set['x_vals'], test_set['y_vals'], color='brown')
 ax[1, 1].plot(x_values_for_plots, y_values_for_plot_test_RIDGE)
 ax[1, 1].set_title('Test Set with RIDGE Regression')
+ax[1, 1].text(0, 10, 'MSE: ' + str('%.4f' % current_error_RIDGE_test), style='italic',
+              bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 3})
 
-errors_MLE = []
+plt.savefig('plots_with_MSE.png')
+plt.show()
+
+errors_MLE_training = []
 for degree in range(0, 20, 1):
-    error_MLE = calculate_error(test_set, calculate_MLE_coefficients(degree), degree)
-    errors_MLE.append(error_MLE)
-
-ax[2, 0].set_title('Error in Relation to Polynomial Degree')
-ax[2, 0].plot(range(0, 20), errors_MLE, marker='o')
-
-plt.show()
+    current_error_MLE = calculate_error(training_set, calculate_MLE_coefficients(degree), degree)
+    errors_MLE_training.append(current_error_MLE)
 
 
-errors_RIDGE = []
-for degree in range(2, 20, 1):
-    err = []
-    for epsilon in range(0, 30):
-        error_RIDGE = calculate_error(test_set, calculate_RIDGE_coefficients(degree, epsilon/20), degree)
-        err.append(error_RIDGE)
-    errors_RIDGE.append(err)
+fig1, ax1 = plt.subplots(1, 2)
+ax1[0].set_title('MSE and Degree - Training Set')
+ax1[0].plot(range(0, 20), errors_MLE_training, marker='o')
+ax1[0].set_xlabel('Degree')
+ax1[0].set_ylabel('MSE')
 
+errors_MLE_test = []
+for degree in range(0, 20, 1):
+    current_error_MLE = calculate_error(test_set, calculate_MLE_coefficients(degree), degree)
+    errors_MLE_test.append(current_error_MLE)
 
-fig2 = plt.figure()
-ax2 = plt.axes(projection='3d')
+ax1[1].set_title('MSE and Degree - Test Set')
+ax1[1].plot(range(0, 20), errors_MLE_test, marker='o')
+ax1[1].set_xlabel('Degree')
+ax1[1].set_ylabel('MSE')
 
-x_axis = []
-y_axis = []
-z_axis = []
-degree = 0
-for deg in errors_RIDGE:
-    val = 0
-    for value in deg:
-        x_axis.append(degree)
-        z_axis.append(value)
-        y_axis.append(val/100)
-        val += 1
-    degree += 1
-
-plt.xlabel('Degree')
-plt.ylabel('Epsilon')
-
-ax2.scatter3D(x_axis, y_axis, z_axis)
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=1.5, wspace=None, hspace=None)
+plt.savefig('MSE_plots.png')
 plt.show()
 
 
 
-print("MSE for MLE and training Set is:")
-print(calculate_error(training_set, calculate_MLE_coefficients()))
 
-print("MSE for MLE and test Set is:")
-print(calculate_error(test_set, calculate_MLE_coefficients()))
 
-print("\nMSE for RIDGE and training Set is:")
-print(calculate_error(training_set, calculate_RIDGE_coefficients()))
 
-print("MSE for RIDGE and test Set is:")
-print(calculate_error(test_set, calculate_RIDGE_coefficients()))
 
-# Plot MSE in relation to polynomial degree
+
 
